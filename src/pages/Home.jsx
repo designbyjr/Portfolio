b901d4689717d28df1a51dcb6935fdddf639a508
Jamie Design BY JR <one4allplugin@gmail.com>
2025-09-07 18:34:22 +0100

    style(Home): add container class to root div and clean up CSS
    remove redundant theme comments and add utility classes for common styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="min-h-screen ">
+    <div className="min-h-screen container">
       {/* Theme  Toggle */}
 
       {/*Background effects */}
